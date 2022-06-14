7edc8758aa0b8ea68e78aa001c2b954600f78760
julcik <julciks@gmail.com>
2022-06-14 19:32:19 +0100

    click added

--- scripts/train.py
+++ scripts/train.py
@@ -3,6 +3,7 @@ from typing import  Optional
 import pytorch_lightning as pl
 from gitract.data.data_module import LitDataModule
 from gitract.model.runner import LitModule
+import click
 
 KAGGLE_DIR = Path("/") / "kaggle"
 INPUT_DIR = KAGGLE_DIR / "input"
@@ -11,12 +12,11 @@ OUTPUT_DIR = KAGGLE_DIR / "working"
 INPUT_DATA_DIR = INPUT_DIR / "uw-madison-gi-tract-image-segmentation"
 INPUT_DATA_NPY_DIR = INPUT_DIR / "uw-madison-gi-tract-image-segmentation-masks"
 
-SPATIAL_SIZE = (192, 192, 128)
 N_SPLITS = 5
 RANDOM_SEED = 42
 VAL_FOLD = 0
-BATCH_SIZE = 8
-NUM_WORKERS = 0 #2
+BATCH_SIZE = 32
+NUM_WORKERS = 6
 OPTIMIZER = "Adam"
 LEARNING_RATE = 2e-3
 WEIGHT_DECAY = 1e-6
@@ -24,7 +24,7 @@ SCHEDULER = None
 MIN_LR = 1e-6
 
 FAST_DEV_RUN = False # Debug training
-GPUS = 1
+GPUS = 0
 MAX_EPOCHS = 30
 PRECISION = 32
 
@@ -33,27 +33,32 @@ THR = 0.45
 
 DEBUG = False # Debug complete pipeline
 
-
+@click.command()
+@click.option('--data_path', default="../mmseg_train/splits/fold_0.csv")
+@click.option('--holdout_path', default="../mmseg_train/splits/holdout_0.csv")
+@click.option('--batch_size', default=BATCH_SIZE)
+@click.option('--num_workers', default=NUM_WORKERS)
+@click.option('--learning_rate', default=LEARNING_RATE)
+@click.option('--max_epochs', default=MAX_EPOCHS)
 def train(
-        random_seed: int = RANDOM_SEED,
-        data_path: str = "../mmseg_train/splits/fold_0.csv",
-        holdout_path: str = "../mmseg_train/splits/holdout_0.csv",
-        batch_size: int = BATCH_SIZE,
-        num_workers: int = NUM_WORKERS,
-        learning_rate: float = LEARNING_RATE,
+
+        data_path: str,
+        holdout_path: str,
+        batch_size: int,
+        num_workers: int,
+        learning_rate: float,
         weight_decay: float = WEIGHT_DECAY,
         scheduler: Optional[str] = SCHEDULER,
         min_lr: float = MIN_LR,
+        device: str = DEVICE,
         gpus: int = GPUS,
         fast_dev_run: bool = FAST_DEV_RUN,
         max_epochs: int = MAX_EPOCHS,
         precision: int = PRECISION,
         debug: bool = DEBUG,
+        random_seed: int = RANDOM_SEED,
 ):
     pl.seed_everything(random_seed)
-
-    if debug:
-        max_epochs = 2
 
     data_module = LitDataModule(
         data_path=data_path,
@@ -73,7 +78,8 @@ def train(
 
     trainer = pl.Trainer(
         # fast_dev_run=fast_dev_run,
-        # gpus=gpus,
+        accelerator=device,
+        gpus=gpus,
         log_every_n_steps=10,
         logger=pl.loggers.CSVLogger(save_dir='logs/'),
         max_epochs=max_epochs,
