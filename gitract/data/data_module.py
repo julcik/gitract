212b6af1588de889bc20542811c496cd4ad0a3f5
julcik <julciks@gmail.com>
2022-06-29 22:43:22 +0100

    shuffle before split

--- gitract/data/data_module.py
+++ gitract/data/data_module.py
@@ -85,7 +85,7 @@ class LitDataModule(pl.LightningDataModule):
 
         if data_path:
             df = parse_train(data_path,  slices=slices, stride = stride)
-            df = df.sample(frac=1.0, replace=False, random_state=42)
+            df = df.sample(frac=1.0, replace=False, random_state=42).reset_index(drop=True)
 
             from sklearn.model_selection import GroupKFold
             split = list(GroupKFold(5).split(df["patient"], groups=df["patient"]))
@@ -145,6 +145,141 @@ class LitDataModule(pl.LightningDataModule):
             monai.transforms.Resized(keys=["image", "masks"], size_mode="longest", spatial_size=spatial_size[0],
                                      mode="nearest"),
             monai.transforms.ResizeWithPadOrCropd(keys=["image", "masks"], spatial_size=spatial_size),
+            monai.transforms.ToTensord(keys=["image", "masks"]),
+        ]
+
+        test_transforms = [
+            monai.transforms.LoadImaged(keys=["image", "masks"], reader=GitractReader(), allow_missing_keys=True),
+            monai.transforms.ScaleIntensityd(keys="image", minv=None, maxv=None, factor=1 / 255.0 - 1,
+                                             allow_missing_keys=True),
+            monai.transforms.NormalizeIntensityd(keys="image", subtrahend=mean, divisor=std, channel_wise=True,
+                                                 allow_missing_keys=True),
+            monai.transforms.Resized(keys=["image", "masks"], size_mode="longest", spatial_size=spatial_size,
+                                     mode="nearest", allow_missing_keys=True),
+            monai.transforms.ResizeWithPadOrCropd(keys=["image", "masks"], spatial_size=(spatial_size, spatial_size),
+                                                  allow_missing_keys=True),
+            monai.transforms.ToTensord(keys=["image", "masks"], allow_missing_keys=True),
+        ]
+
+        train_transforms = monai.transforms.Compose(transforms)
+        val_transforms = monai.transforms.Compose(val_transforms)
+        test_transforms = monai.transforms.Compose(test_transforms)
+
+        return train_transforms, val_transforms, test_transforms
+
+    def train_dataloader(self) -> DataLoader:
+        return self._dataloader(self.train_dataset, train=True)
+
+    def val_dataloader(self) -> DataLoader:
+        return self._dataloader(self.val_dataset)
+
+    def _dataloader(self, dataset: CSVDataset, train: bool = False) -> DataLoader:
+        return DataLoader(
+            dataset,
+            batch_size=self.hparams.batch_size,
+            shuffle=train,
+            drop_last=train,
+            num_workers=self.hparams.num_workers,
+            pin_memory=True,
+        )
+
+
+class LitDataModule3d(LitDataModule):
+    def __init__(
+        self,
+        data_path: Optional[PathLike],
+        spatial_size: Tuple[int,int],
+        batch_size: int,
+        num_workers: int,
+        fold: int = 0,
+
+    ):
+        super().__init__()
+        set_determinism(seed=42)
+        self.save_hyperparameters()
+        self.data_pat = data_path
+        self.fold = fold
+
+        train_transforms, val_transforms, test_transforms = self._init_transforms()
+
+        if data_path:
+            df = parse_train(data_path, slices=-11, stride=1)
+            df = df.sample(frac=1.0, replace=False, random_state=42).reset_index(drop=True)
+
+            from sklearn.model_selection import GroupKFold
+            split = list(GroupKFold(5).split(df["patient"], groups=df["patient"]))
+            train_idx, valid_idx = split[self.fold]
+
+            self.df_train = df.iloc[train_idx]
+            self.df_val = df.iloc[valid_idx]
+
+            self.train_dataset = CSVDataset(self.df_train[["image", "masks"]],
+                              col_names=["image", "masks"],
+                              transform=train_transforms
+                              )
+            #CSVDataset(src=data_path, col_names=["image", "masks"], transform=train_transforms)
+            self.val_dataset = CSVDataset(self.df_val[["image", "masks"]],
+                              col_names=["image", "masks"],
+                              transform=val_transforms
+                              )
+
+            # for d in self.train_dataset:
+            #     print(d)
+
+
+    def _init_transforms(self):
+        spatial_size = self.hparams.spatial_size
+
+        mean = np.array([0.456]*self.hparams.slices)
+        std = np.array([0.224]*self.hparams.slices)
+
+        transforms = [
+            # monai.transforms.LoadImaged(keys=["image", "masks"]),
+            monai.transforms.LoadImaged(keys=["image", "masks"], reader=GitractReader(), image_only=True),
+            # monai.transforms.AsChannelFirstd(keys=["masks"], channel_dim=2),
+            monai.transforms.Orientationd(keys=["image", "masks"], axcodes="RAS"),
+            monai.transforms.Spacingd(keys=["image", "masks"], pixdim=(
+                1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+            monai.transforms.ScaleIntensityd(keys="image", minv=None, maxv= None, factor=1/255.0 - 1),
+            monai.transforms.NormalizeIntensityd(keys="image", subtrahend=mean, divisor=std, channel_wise=True),
+
+            monai.transforms.RandAdjustContrastd(keys=["image"], prob=0.2),
+            monai.transforms.RandGaussianNoised(keys=["image"], prob=0.1),
+            # monai.transforms.RandFlipd(keys=["image", "masks"], prob=0.5),
+            monai.transforms.RandRotate90d(keys=["image", "masks"], prob=0.5),
+            monai.transforms.RandRotated(keys=["image", "masks"], range_x=5, range_y=0, prob=0.2),
+            monai.transforms.RandZoomd(keys=["image", "masks"], prob=0.2, min_zoom=0.8, max_zoom=1.2),
+            monai.transforms.Rand2DElasticd(keys=["image", "masks"], magnitude_range=(0, 1), spacing=(0.3, 0.3),
+                                            prob=0.3),
+
+            monai.transforms.CropForegroundd(keys=["image", "masks"], source_key="image"),
+            monai.transforms.RandCropByPosNegLabeld(
+                keys=["image", "masks"],
+                label_key="masks",
+                spatial_size=(spatial_size[0],spatial_size[0],spatial_size[0]),
+                pos=1,
+                neg=1,
+                num_samples=4,
+                image_key="image",
+                image_threshold=0,
+            ),
+
+
+            monai.transforms.ToTensord(keys=["image", "masks"]),
+        ]
+
+        val_transforms = [
+            monai.transforms.LoadImaged(keys=["image", "masks"], reader=GitractReader(), image_only=True),
+            # monai.transforms.LoadImaged(keys=["image", "masks"]),
+            # monai.transforms.AddChanneld(keys=["image", "masks"]),
+            # monai.transforms.AsChannelFirstd(keys=["masks"], channel_dim=2),
+            monai.transforms.Orientationd(keys=["image", "masks"], axcodes="RAS"),
+            monai.transforms.Spacingd(keys=["image", "masks"], pixdim=(
+                1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+            monai.transforms.ScaleIntensityd(keys="image", minv=None, maxv=None, factor=1 / 255.0 - 1),
+            monai.transforms.NormalizeIntensityd(keys="image", subtrahend=mean, divisor=std, channel_wise=True),
+            # monai.transforms.ResizeWithPadOrCrop(keys=["image_3d"], spatial_size=spatial_size),
+            monai.transforms.CropForegroundd(keys=["image", "masks"], source_key="image"),
             monai.transforms.ToTensord(keys=["image", "masks"]),
         ]
 
